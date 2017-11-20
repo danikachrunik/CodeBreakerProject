322b29f8192ad798d971999de65657c95171205d
Danika <danikachrunik@gmail.com>
2017-11-19 22:08:52 -0800

    first pass
    added most js

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,66 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+
+    if(answer === '' || attempt === '') {
+      setHiddenfields();
+    }
+
+    if(!validateInput(input.value)) {
+      return false;
+    } else {
+      attempt++;
+    }
+
+    if(getResults(input)) {
+      setMessage( "You Win! :)" );
+      showAnswer(true);
+      showReplay();
+    } else if (!getResults() && attempt >= 10) {
+      setMessage( "You Lose! :(" );
+      showAnswer(false);
+      showReplay();
+    } else {
+      setMessage("Incorrect, try again.");
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+  attempt = 0;
+  answer = Math.floor( Math.random() * 9999 ).toString();
+
+  while(answer.length < 4) {
+    answer = "0" + answer;
+  }
+}
+
+function setMessage(newMessage) {
+  document.getElementById('message').innerHTML = newMessage;
+}
+
+function validateInput(input) {
+  if(input.length === 4) {
+    return true;
+  } else {
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+}
+
+function getResults(input) {
+  document.getElementById('results').innerHTML = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+
+  let numCorrect = 4;
+
+  numCorrect === 4 ? true : false;
+}
+
+function showAnswer(input) {
+  let codeElem = document.getElementById('code');
+  codeElem.innerHTML(answer);
+  input ? codeelem.addClass(' success') : codeElem.addClass(' failure');
+}
+
+function showReplay() {
+  document.getElementById('guessing-div').css({display: "block"});
+}
